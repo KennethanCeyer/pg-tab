a82c916a6232ff3db0e804268aebbdf4e17b5d57
Kenneth.an Ceyer <kennethan@nhpce.com>
2014-05-19 15:06:09 +0900

    PIGNOSE-Tab version 1.3 updates

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ### Start
 'PIGNOSE Tab' help to make to complecate tab structures easily
 
-This plugin version is ```1.1```
+This plugin version is ```1.3```
 
 ### Type
 + Normal Tab
@@ -11,8 +11,9 @@ This plugin version is ```1.1```
 + Article Tab
 + Styled Tab
 + Response Tab
++ Mobile Tab
 
 ### Installation
 1. Download sources form ```Github```
 2. Import ```jquery-1.11.0.js``` and ```jquery.PignoseTab.js```
-3. Insert js inline ```$(jQuery element).pignoseTab();```
+3. Insert js inline ```$(Tab jQuery Selector).pignoseTab();```
--- js/jquery.pignoseTab.js
+++ js/jquery.pignoseTab.js
@@ -1,9 +1,9 @@
 /*
  *
- *   PIG NOSE TAB 1.1
+ *   PIG NOSE TAB 1.3
  *   ================
  *
- *   @ date         2014-03-31
+ *   @ date         2014-05-19
  *   @ author       sungmin. han (http://kennethan.dothome.co.kr)
  *   @ copyright    pig-nose
  *   @ license      'MIT license' - (http://rem.mit-license.org)
@@ -21,10 +21,29 @@
 			var $ul   = $elem.children('ul').addClass('pignose_tab_group');
 			var $li   = $ul.children('li').addClass('pignose_tab_list');
 			var $btn  = $li.children('a:not(".pignose_tab_more")').addClass('pignose_tab_btn');
+			var $more = $li.children('a.pignose_tab_more');
 			var $con  = $li.children('div').addClass('pignose_tab_con');
+
+			if($elem.hasClass('pignose_tab_responsive_btn'))
+			{
+				$(window).bind('resize', function() {
+					var margin = 0;
+					if($more.length)
+					{
+						margin = ($elem.width() - $more.outerWidth() + $more.offset().left);
+					}
+
+					$btn.each(function() {
+						$(this).outerWidth(parseInt(($elem.width() - margin) / $btn.length));
+						$(this).parent().width($(this).width());
+					});
+				});
+
+				$(window).trigger('resize');
+			}
 			
-			if($li.siblings('.active').length < 1) $li.eq(0).addClass('active');
-			$li.siblings('.active').children('.pignose_tab_btn, .pignose_tab_con').addClass('active');
+			if($li.filter('.active').length < 1) $li.eq(0).addClass('active');
+			$li.filter('.active').children('.pignose_tab_btn, .pignose_tab_con').addClass('active');
 
 			$elem.height($con.filter('.active').position().top + $con.filter('.active').outerHeight());
 			$li.filter(':last').children('.pignose_tab_btn').addClass('last_btn');
@@ -34,6 +53,7 @@
 				{
 					$li.removeClass('active').children('.pignose_tab_btn, .pignose_tab_con').removeClass('active');
 					$(this).parent().addClass('active').children('.pignose_tab_btn, .pignose_tab_con').addClass('active');
+					$elem.height($(this).next().position().top + $(this).next().outerHeight());
 				}
 				event.preventDefault();
 			});
