bd08ceae927a4ba1378123a3dfb4688485c28f59
Kenneth.an Ceyer <kennethan@nhpce.com>
2014-07-11 12:58:28 +0900

    PIGNOSE-Tab version 2.0 updates

--- README.md
+++ README.md
@@ -1,19 +1,31 @@
 #####################################################################################################################
-## PIGNOSE Tab
-### Start
-'PIGNOSE Tab' help to make to complecate tab structures easily
+# PIGNOSE Tab
 
-This plugin version is ```1.3```
+## Description
+`Pignose Tab` would be helping to make to complecate tab structures easily with html
+This plugin's lastest version is `2.0`
 
-### Type
+
+Pignose support many type tabs, please see the exhibition.
 + Normal Tab
 + Sub Tab
++ Four depth Tab (added version 2.0)
 + Article Tab
 + Styled Tab
 + Response Tab
-+ Mobile Tab
++ Mobile Tab (added version 1.4)
 
-### Installation
-1. Download sources form ```Github```
-2. Import ```jquery-1.11.0.js``` and ```jquery.PignoseTab.js```
-3. Insert js inline ```$(Tab jQuery Selector).pignoseTab();```
+## Installation
+1. Download <b>Pignose Tab</b> plugin
+2. Import <b>jquery-1.11.0.js</b> and <b>jquery.PignoseTab.js</b> on your html documents
+3. Insert js inline ```$(jQuery Selector).pignoseTab();```
+
+## Settings
+> Pignose support tab animation
+
+| Attribute       | Default      | Values                          | Description                                           |
+|:----------------|:-------------|:--------------------------------|:------------------------------------------------------|
+| callback        | null         | function                        | If tab button clicked, this function called always    |
+| animation       | static       | static, true, false             | Switch to use animation, `static` is same `false`     |
+| animationTime   | 300          | Millisecond time                | If you use animation, this can control animation time |
+| animationEasing |easeInOutCubic| Easing names                    | If you use animation, this can control animation motion easing, but it need easing plugin to be use, If you not, that use linear easing by default |
--- js/jquery.pignoseTab.js
+++ js/jquery.pignoseTab.js
@@ -1,9 +1,9 @@
 /*
  *
- *   PIG NOSE TAB 1.4
+ *   PIG NOSE TAB 2.0
  *   ================
  *
- *   @ date         2014-05-19
+ *   @ date         2014-07-11
  *   @ author       sungmin. han (http://kennethan.dothome.co.kr)
  *   @ copyright    pig-nose
  *   @ license      'MIT license' - (http://rem.mit-license.org)
@@ -12,54 +12,129 @@
 
 (function($) {
 	$.fn.pignoseTab = function(options) {
-		var settings = $.extend({
+		var _this    = this;
+
+		this.settings = $.extend({
+			'callback'        : null,
+			'animation'       : 'static',
+			'animationTime'   : 300,
+			'animationEasing' : 'easeInOutCubic'
 		}, options);
 
-		return this.each(function(idx, elem) {
-			var _this = this;
-			var $elem = $(elem);
-			var $ul   = $elem.children('ul').addClass('pignose_tab_group');
-			var $li   = $ul.children('li').addClass('pignose_tab_list');
-			var $btn  = $li.children('a:not(".pignose_tab_more")').addClass('pignose_tab_btn');
-			var $more = $li.children('a.pignose_tab_more');
-			var $con  = $li.children('div').addClass('pignose_tab_con');
+		this.rendering = function(root, flag) {
+			root.each(function(idx, elem) {
+				if(typeof elem === 'undefined' || elem.length < 1) return;
+				var $elem = $(elem);
+				var $root = $elem.parents('.pignose_tab_wrap:last').addClass('pignose_tab_wrap_root');
+				var $ul   = $elem.children('ul').addClass('pignose_tab_group');
+				var $li   = $ul.children('li').addClass('pignose_tab_list');
+				var $btn  = $li.children('a:not(".pignose_tab_more")').addClass('pignose_tab_btn');
+				var $more = $li.children('a.pignose_tab_more');
+				var $con  = $li.children('div').addClass('pignose_tab_con');
+				
+				if($root.length < 1) $root = $elem.addClass('pignose_tab_wrap_root');
 
-			if($elem.hasClass('pignose_tab_responsive_btn'))
-			{
-				$(window).bind('resize', function() {
-					var margin = 0;
-					if($more.length)
-					{
-						margin = ($elem.width() - ($more.innerWidth(true) + $more.offset().left));
-					}
+				if($con.children('.pignose_tab_wrap').length > 0) {
+					_this.rendering($con.children('.pignose_tab_wrap'));
+				}
 
-					$btn.each(function() {
-						console.log($elem.innerWidth());
-						$(this).outerWidth(parseInt(($elem.width() - margin) / $btn.length));
-						$(this).parent().width($(this).outerWidth(true));
+				if($elem.hasClass('pignose_tab_responsive_btn'))
+				{
+					$(window).bind('resize', function() {
+						var margin = 0;
+
+						if($more.length)
+						{
+							margin = ($elem.width() - ($more.innerWidth(true) + $more.offset().left));
+						}
+
+						$btn.each(function() {
+							$(this).outerWidth(parseInt(($elem.width() - margin) / $btn.length));
+							$(this).parent().width($(this).outerWidth(true));
+						});
 					});
+				}
+				
+				if($li.filter('.active').length < 1) $li.eq(0).addClass('active');
+				$li.filter('.active').children('.pignose_tab_btn, .pignose_tab_con').addClass('active');
+				$li.filter(':last').children('.pignose_tab_btn').addClass('last_btn');
+
+				$btn.bind('click', function(event) {
+					if(!$(this).hasClass('active'))
+					{
+						$li.removeClass('active').children('.pignose_tab_btn, .pignose_tab_con').removeClass('active');
+						$(this).parent().addClass('active').children('.pignose_tab_btn, .pignose_tab_con').addClass('active');
+						_this.resizeTab($root);
+						$(window).trigger('resize');
+
+						if(_this.settings.callback != null && typeof _this.settings === 'function') {
+							_this.settings.callback();
+						}
+					}
+					event.preventDefault();
 				});
 
-				$(window).trigger('resize');
-				$(window).bind('load', function() { $(this).trigger('resize'); });
-			}
-			
-			if($li.filter('.active').length < 1) $li.eq(0).addClass('active');
-			$li.filter('.active').children('.pignose_tab_btn, .pignose_tab_con').addClass('active');
-
-			$elem.height($con.filter('.active').position().top + $con.filter('.active').outerHeight());
-			$li.filter(':last').children('.pignose_tab_btn').addClass('last_btn');
-
-			$btn.bind('click', function(event) {
-				if(!$(this).hasClass('active'))
-				{
-					$li.removeClass('active').children('.pignose_tab_btn, .pignose_tab_con').removeClass('active');
-					$(this).parent().addClass('active').children('.pignose_tab_btn, .pignose_tab_con').addClass('active');
-					$elem.height($(this).next().position().top + $(this).next().outerHeight());
+				if(typeof flag !== 'undefined') {
+					_this.resizeTab($elem, 'initialization');
 					$(window).trigger('resize');
+					$(window).bind('load', function() { $(this).trigger('resize'); });
 				}
-				event.preventDefault();
 			});
-		});
+			return root;
+		};
+
+		this.resizeTab = function(elem, idx, type) {
+			var renderingHeight;
+			var renderingType       = type;
+			var $elem               = elem;
+			var $activeTabContent   = $elem.filter('.active');
+			var $nearTabContent     = $elem.find('.pignose_tab_group').eq(0).find('> .pignose_tab_list.active > .pignose_tab_con');
+			var $siblingsTabContent = $elem.siblings('.pignose_tab_con')
+			var $childrenTabContent = $elem.find('.pignose_tab_wrap').eq(0).find('> .pignose_tab_group > .pignose_tab_list.active > .pignose_tab_con');
+
+			if(typeof idx == 'undefined') idx = 0;
+			if(typeof idx == 'string' && idx == 'initialization') { renderingType = 'quick'; idx = 0; }
+			if(typeof $elem === 'undefined' || $elem.length < 1) return false;
+
+			if($elem.is('.pignose_tab_con') === false) {
+				if($nearTabContent.length > 0) {
+					_this.resizeTab($nearTabContent, idx, renderingType);
+				} else {
+					_this.resizeTab($siblingsTabContent, idx, renderingType);
+				}
+				return false;
+			}
+
+			if($childrenTabContent.length > 0) {
+				renderingHeight = _this.resizeTab($childrenTabContent, idx + 1, renderingType);
+			}
+
+			if($activeTabContent.length > 0) {
+				if(renderingHeight == null || renderingHeight <= 0) renderingHeight = parseInt($activeTabContent.outerHeight());
+				if(typeof _this.settings.animation === 'undefined' ||
+				   !!_this.settings.animation      === false       ||
+				   _this.settings.animation        ==  'static'    ||
+				   renderingType                   ==  'quick'
+				) {
+					$activeTabContent.parents('.pignose_tab_wrap').eq(0).height(parseInt($activeTabContent.position().top) + parseInt($activeTabContent.outerHeight()));
+				}
+				else {
+					$activeTabContent.parents('.pignose_tab_wrap').eq(0)
+					.stop()
+					.animate(
+						{
+							'height' : (parseInt($activeTabContent.position().top) + renderingHeight) + 'px'
+						},
+						_this.settings.animationTime,
+						(typeof window[_this.settings.animationEasing] !== 'undefined'? _this.settings.animationEasing:null)
+					);
+				}
+				renderingHeight += parseInt($activeTabContent.position().top);
+			}
+
+			return renderingHeight;
+		};
+
+		return _this.rendering(this, true);
 	};
 })(jQuery)
